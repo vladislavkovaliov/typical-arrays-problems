b4fdb7c4c8933884be8fd160a776a79b758565f4
Vladislav Kovaliov <v.v.kovaliov@gmail.com>
2020-03-09 19:15:19 +0300

    master: adds solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array = []) {
+  return array.reduce(
+    (acc, item) => item < acc ? item : acc,
+    array[0] || 0,
+  );
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array = []) {
+  return array.reduce(
+    (acc, item) => item > acc ? item : acc,
+    array[0] || 0,
+  );
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array = []) {
+  return array.reduce(
+    (acc, item, _, arr) => (acc * 10 + +((item / arr.length) * 10)) / 10,
+    0,
+  );
 }
+
